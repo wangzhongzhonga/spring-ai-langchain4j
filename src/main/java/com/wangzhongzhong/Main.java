24a714bbac73122ad80753b5026e05a96b2f1044
WangZhongzhong <2528957125@qq.com>
2026-07-10 11:35:13 +0800

    添加相关依赖

--- src/main/java/com/wangzhongzhong/Main.java
+++
@@ -1,8 +0,0 @@
-package com.wangzhongzhong;
-
-public class Main {
-    //主入口222
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/wangzhongzhong/java/ai/langchain4j/ZhihuiYLApp.java
@@ -0,0 +1,12 @@
+package com.wangzhongzhong.java.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuiYLApp {
+    //主入口222
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuiYLApp.class, args);
+    }
+}
